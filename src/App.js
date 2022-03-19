f948a9305d0db7d835c254108bdb37292efda97e
Scott Green <sagreenxyz@gmail.com>
2022-03-19 14:06:47 -0500

    7. rendering the counter in multiple places same page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   let [ counter, changeCounter ] = useState(1);
   window.changeCounter = changeCounter;
   return (
-    <h1>{counter}</h1>
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
   )
 };
 
