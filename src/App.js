ecbe444a03018c0f44319fa464106d5a0beae881
Scott Green <sagreenxyz@gmail.com>
2022-03-20 20:52:36 -0500

    4. making changeCounter accessible from web browser developer console

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 function App() {
   let [ counter, changeCounter ] = useState(1)
+  window.changeCounter = changeCounter
   return (
     <h1>{counter}</h1>
   );
